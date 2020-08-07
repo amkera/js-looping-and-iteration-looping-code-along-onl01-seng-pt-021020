8a294f708d65c957988e9feef035699e00507327
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-07 19:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const eventName = "birthday";
 
 function writeCards(names, eventName) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]} for the wonderful birthday gift!`); 
+    console.log(`Thank you, ${names[i]} for the wonderful surprise birthday gift!`); 
     
   }
   return names; 
