297946586dcc0f4f589771de46720c3874e0c0eb
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-07 19:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
+
+const names = 
+
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) {
+    
+  }
+}
