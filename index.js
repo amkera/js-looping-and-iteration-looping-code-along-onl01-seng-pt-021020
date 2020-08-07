2647a4f9ed6e278f96733db6aa8417e708e9bd47
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-07 19:45:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
- 
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
- 
-  return gifts;
-}
- 
-wrapGifts(gifts);
 
-const names = 
+const names = ["Ada", "Brendan", "Ali"];
+const eventName = "birthday"; 
 
 function writeCards(names, eventName) {
   for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]} for the wonderful birthday gift!`); 
     
   }
+  return names; 
 }
+
+writeCards(names); 
